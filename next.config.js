38254604643d156f8a56f841e9217c9d8c120c78
Brandon Bayer <b@bayer.ws>
2018-02-06 16:09:20 -0500

    chore: remove uglify override because of Next.js 5.0

--- next.config.js
+++ next.config.js
@@ -1,23 +1,5 @@
-// Fix uglify solution from https://github.com/zeit/next.js/pull/3150#issuecomment-346755136
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
   webpack: config => {
-    for (let index = 0; index < config.plugins.length; index += 1) {
-      if (config.plugins[index].constructor.name === 'UglifyJsPlugin') {
-        // Delete the UglifyJS plugin
-        // And add uglify-es plugin (https://github.com/webpack-contrib/uglifyjs-webpack-plugin)
-        config.plugins.splice(
-          index,
-          1,
-          new UglifyJSPlugin({
-            sourceMap: false,
-            parallel: true,
-          })
-        )
-        break
-      }
-    }
-
     // Had to add when using algolia SSR
     if (config.resolve.alias) {
       delete config.resolve.alias.react
@@ -27,18 +9,3 @@ module.exports = {
     return config
   },
 }
-
-// module.exports = {
-//   webpack: (config, {buildId, dev}) => {
-//     // Perform customizations to webpack config
-//
-//     // Important: return the modified config
-//     return config
-//   },
-//   webpackDevMiddleware: config => {
-//     // Perform customizations to webpack dev middleware config
-//
-//     // Important: return the modified config
-//     return config
-//   },
-// }
