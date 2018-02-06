0a02b6893bade11180b6aeec605819fe987d5292
Brandon Bayer <b@bayer.ws>
2018-02-06 10:03:51 -0500

    new: remove hotjar

--- components/TrackingScripts.js
+++ components/TrackingScripts.js
@@ -40,21 +40,21 @@ const IntercomScript = () => (
   />
 )
 
-const HotJarScript = () => (
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:756025,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-`,
-    }}
-  />
-)
+// const HotJarScript = () => (
+//   <script
+//     dangerouslySetInnerHTML={{
+//       __html: `(function(h,o,t,j,a,r){
+//         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+//         h._hjSettings={hjid:756025,hjsv:6};
+//         a=o.getElementsByTagName('head')[0];
+//         r=o.createElement('script');r.async=1;
+//         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+//         a.appendChild(r);
+//     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+// `,
+//     }}
+//   />
+// )
 
 type Props = {
   production: boolean,
@@ -66,7 +66,6 @@ const TrackingScripts = ({production}: Props) =>
       <TrackJsScript />
       <IntercomScript />
       <GoogleAnalyticsScript />
-      <HotJarScript />
     </React.Fragment>
   ) : (
     <React.Fragment>
