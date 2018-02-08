c80956cf6234e5a1165819bc0b97d78d816c9116
Brandon Bayer <b@bayer.ws>
2018-02-08 09:19:57 -0500

    chore: upgrade next and fix react production error

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   webpack: config => {
-    // Had to add when using algolia SSR
-    if (config.resolve.alias) {
-      delete config.resolve.alias.react
-      delete config.resolve.alias['react-dom']
-    }
-
     return config
   },
 }
