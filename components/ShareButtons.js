81a13e1174985344a0e5bfaee93a97322b35fb4c
Brandon Bayer <b@bayer.ws>
2018-04-07 16:58:23 -0400

    new: remove unneeded social sharing buttons

--- components/ShareButtons.js
+++ components/ShareButtons.js
@@ -285,27 +285,6 @@ export default (props: Props) => (
       </div>
     </a>
 
-    {/* Sharingbutton Google+ */}
-    <a
-      className="resp-sharing-button__link"
-      href="https://plus.google.com/share?url=https%3A%2F%2Fglutenproject.com"
-      target="_blank" //eslint-disable-line react/jsx-no-target-blank
-      rel="noopener"
-      title="Share to Google+"
-      aria-label=""
-    >
-      <div className="resp-sharing-button resp-sharing-button--google resp-sharing-button--small">
-        <div
-          aria-hidden="true"
-          className="resp-sharing-button__icon resp-sharing-button__icon--solid"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M11.37 12.93c-.73-.52-1.4-1.27-1.4-1.5 0-.43.03-.63.98-1.37 1.23-.97 1.9-2.23 1.9-3.57 0-1.22-.36-2.3-1-3.05h.5c.1 0 .2-.04.28-.1l1.36-.98c.16-.12.23-.34.17-.54-.07-.2-.25-.33-.46-.33H7.6c-.66 0-1.34.12-2 .35-2.23.76-3.78 2.66-3.78 4.6 0 2.76 2.13 4.85 5 4.9-.07.23-.1.45-.1.66 0 .43.1.83.33 1.22h-.08c-2.72 0-5.17 1.34-6.1 3.32-.25.52-.37 1.04-.37 1.56 0 .5.13.98.38 1.44.6 1.04 1.84 1.86 3.55 2.28.87.23 1.82.34 2.8.34.88 0 1.7-.1 2.5-.34 2.4-.7 3.97-2.48 3.97-4.54 0-1.97-.63-3.15-2.33-4.35zm-7.7 4.5c0-1.42 1.8-2.68 3.9-2.68h.05c.45 0 .9.07 1.3.2l.42.28c.96.66 1.6 1.1 1.77 1.8.05.16.07.33.07.5 0 1.8-1.33 2.7-3.96 2.7-1.98 0-3.54-1.23-3.54-2.8zM5.54 3.9c.33-.38.75-.58 1.23-.58h.05c1.35.05 2.64 1.55 2.88 3.35.14 1.02-.08 1.97-.6 2.55-.32.37-.74.56-1.23.56h-.03c-1.32-.04-2.63-1.6-2.87-3.4-.13-1 .08-1.92.58-2.5zM23.5 9.5h-3v-3h-2v3h-3v2h3v3h2v-3h3" />
-          </svg>
-        </div>
-      </div>
-    </a>
-
     {/* Sharingbutton Tumblr */}
     <a
       className="resp-sharing-button__link"
@@ -322,26 +301,6 @@ export default (props: Props) => (
         >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path d="M13.5.5v5h5v4h-5V15c0 5 3.5 4.4 6 2.8v4.4c-6.7 3.2-12 0-12-4.2V9.5h-3V6.7c1-.3 2.2-.7 3-1.3.5-.5 1-1.2 1.4-2 .3-.7.6-1.7.7-3h3.8z" />
-          </svg>
-        </div>
-      </div>
-    </a>
-
-    {/* Sharingbutton E-Mail */}
-    <a
-      className="resp-sharing-button__link"
-      href="mailto:?subject=The%20Gluten%20Project%3A%20Find%20any%20certified%20gluten-free%20product&amp;body=https%3A%2F%2Fglutenproject.com"
-      target="_self"
-      title="Share via email"
-      aria-label=""
-    >
-      <div className="resp-sharing-button resp-sharing-button--email resp-sharing-button--small">
-        <div
-          aria-hidden="true"
-          className="resp-sharing-button__icon resp-sharing-button__icon--solid"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M22 4H2C.9 4 0 4.9 0 6v12c0 1.1.9 2 2 2h20c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zM7.25 14.43l-3.5 2c-.08.05-.17.07-.25.07-.17 0-.34-.1-.43-.25-.14-.24-.06-.55.18-.68l3.5-2c.24-.14.55-.06.68.18.14.24.06.55-.18.68zm4.75.07c-.1 0-.2-.03-.27-.08l-8.5-5.5c-.23-.15-.3-.46-.15-.7.15-.22.46-.3.7-.14L12 13.4l8.23-5.32c.23-.15.54-.08.7.15.14.23.07.54-.16.7l-8.5 5.5c-.08.04-.17.07-.27.07zm8.93 1.75c-.1.16-.26.25-.43.25-.08 0-.17-.02-.25-.07l-3.5-2c-.24-.13-.32-.44-.18-.68s.44-.32.68-.18l3.5 2c.24.13.32.44.18.68z" />
           </svg>
         </div>
       </div>
@@ -368,43 +327,21 @@ export default (props: Props) => (
       </div>
     </a>
 
-    {/* Sharingbutton WhatsApp */}
+    {/* Sharingbutton E-Mail */}
     <a
       className="resp-sharing-button__link"
-      href="whatsapp://send?text=The%20Gluten%20Project%3A%20Find%20any%20certified%20gluten-free%20product%20https%3A%2F%2Fglutenproject.com"
-      target="_blank" //eslint-disable-line react/jsx-no-target-blank
-      rel="noopener"
-      title="Share to WhatsApp"
+      href="mailto:?subject=The%20Gluten%20Project%3A%20Find%20any%20certified%20gluten-free%20product&amp;body=https%3A%2F%2Fglutenproject.com"
+      target="_self"
+      title="Share via email"
       aria-label=""
     >
-      <div className="resp-sharing-button resp-sharing-button--whatsapp resp-sharing-button--small">
+      <div className="resp-sharing-button resp-sharing-button--email resp-sharing-button--small">
         <div
           aria-hidden="true"
           className="resp-sharing-button__icon resp-sharing-button__icon--solid"
         >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M20.1 3.9C17.9 1.7 15 .5 12 .5 5.8.5.7 5.6.7 11.9c0 2 .5 3.9 1.5 5.6L.6 23.4l6-1.6c1.6.9 3.5 1.3 5.4 1.3 6.3 0 11.4-5.1 11.4-11.4-.1-2.8-1.2-5.7-3.3-7.8zM12 21.4c-1.7 0-3.3-.5-4.8-1.3l-.4-.2-3.5 1 1-3.4L4 17c-1-1.5-1.4-3.2-1.4-5.1 0-5.2 4.2-9.4 9.4-9.4 2.5 0 4.9 1 6.7 2.8 1.8 1.8 2.8 4.2 2.8 6.7-.1 5.2-4.3 9.4-9.5 9.4zm5.1-7.1c-.3-.1-1.7-.9-1.9-1-.3-.1-.5-.1-.7.1-.2.3-.8 1-.9 1.1-.2.2-.3.2-.6.1s-1.2-.5-2.3-1.4c-.9-.8-1.4-1.7-1.6-2-.2-.3 0-.5.1-.6s.3-.3.4-.5c.2-.1.3-.3.4-.5.1-.2 0-.4 0-.5C10 9 9.3 7.6 9 7c-.1-.4-.4-.3-.5-.3h-.6s-.4.1-.7.3c-.3.3-1 1-1 2.4s1 2.8 1.1 3c.1.2 2 3.1 4.9 4.3.7.3 1.2.5 1.6.6.7.2 1.3.2 1.8.1.6-.1 1.7-.7 1.9-1.3.2-.7.2-1.2.2-1.3-.1-.3-.3-.4-.6-.5z" />
-          </svg>
-        </div>
-      </div>
-    </a>
-
-    {/* Sharingbutton Telegram */}
-    <a
-      className="resp-sharing-button__link"
-      href="https://telegram.me/share/url?text=The%20Gluten%20Project%3A%20Find%20any%20certified%20gluten-free%20product&amp;url=https%3A%2F%2Fglutenproject.com"
-      target="_blank" //eslint-disable-line react/jsx-no-target-blank
-      rel="noopener"
-      title="Share to Telegram"
-      aria-label=""
-    >
-      <div className="resp-sharing-button resp-sharing-button--telegram resp-sharing-button--small">
-        <div
-          aria-hidden="true"
-          className="resp-sharing-button__icon resp-sharing-button__icon--solid"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M.707 8.475C.275 8.64 0 9.508 0 9.508s.284.867.718 1.03l5.09 1.897 1.986 6.38a1.102 1.102 0 0 0 1.75.527l2.96-2.41a.405.405 0 0 1 .494-.013l5.34 3.87a1.1 1.1 0 0 0 1.046.135 1.1 1.1 0 0 0 .682-.803l3.91-18.795A1.102 1.102 0 0 0 22.5.075L.706 8.475z" />
+            <path d="M22 4H2C.9 4 0 4.9 0 6v12c0 1.1.9 2 2 2h20c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zM7.25 14.43l-3.5 2c-.08.05-.17.07-.25.07-.17 0-.34-.1-.43-.25-.14-.24-.06-.55.18-.68l3.5-2c.24-.14.55-.06.68.18.14.24.06.55-.18.68zm4.75.07c-.1 0-.2-.03-.27-.08l-8.5-5.5c-.23-.15-.3-.46-.15-.7.15-.22.46-.3.7-.14L12 13.4l8.23-5.32c.23-.15.54-.08.7.15.14.23.07.54-.16.7l-8.5 5.5c-.08.04-.17.07-.27.07zm8.93 1.75c-.1.16-.26.25-.43.25-.08 0-.17-.02-.25-.07l-3.5-2c-.24-.13-.32-.44-.18-.68s.44-.32.68-.18l3.5 2c.24.13.32.44.18.68z" />
           </svg>
         </div>
       </div>
